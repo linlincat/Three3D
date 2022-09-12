c8ea5f0116bc14ef99d95241279d417a9d9f95ec
cat <none>
2022-09-12 19:21:39 +0800

    双击全屏及退出全屏功能

---
+++ src/main/main.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three'   
+// 导入轨道控制器
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// 导入动画库
+import gsap from 'gsap'
+// 导入dat.gui图形控制器
+import * as dat from 'dat.gui'
+// 创建场景
+const scene = new THREE.Scene()
+//  创建穿透摄像机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+//  设置摄像机位置
+camera.position.set(0,0,10)
+//  将摄像机添加到场景
+scene.add(camera)
+//  创建图形
+const  cubeGeomeetry  =  new THREE.BoxGeometry(1,1,1);
+//  创建材质
+const  cubeMaterial  = new THREE.MeshBasicMaterial({color:0xffff00}) 
+//  融合到网格中国呢
+const  cube  = new THREE.Mesh(cubeGeomeetry,cubeMaterial)
+//  将网格添加到场景中
+scene.add(cube)
+//  创建渲染器
+const  renderer   = new THREE.WebGL1Renderer()
+//  设置渲染器舞台大小
+renderer.setSize(window.innerWidth, window.innerHeight)
+//  添加渲染元素到页面中
+document.body.appendChild(renderer.domElement)
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 设置控制器阻尼,让控制器更有真实感
+controls.enableDamping = true
+// 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper)
+//  将场景与摄像机渲染出来
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement = document.fullscreenElement;
+  if (!fullscreenElement) {
+    renderer.domElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
+  }
+})
+
+function render() {
+  // yoyo gsap动画要求在循环渲染中更新
+  controls.update();
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
+}
+render()
